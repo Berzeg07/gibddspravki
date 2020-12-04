54afd917d2792fa12c22581d7e2b498a9143b781
Aslan <berezg86@gmail.com>
2020-12-04 21:50:53 +0300

    Слайдер с инфой, шапка

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,3 +1,28 @@
 $(document).ready(function () {
-    // alert(1);
+    $('.burger').click(function () {
+        $(this).toggleClass('is-active')
+        $('.dropdown').toggleClass('is-active')
+    })
+
+    var cardSlider = new Swiper('.cards-gallery', {
+        slidesPerView: 2,
+        spaceBetween: 0,
+        loop: true,
+        observer: true,
+        observeParents: true,
+        navigation: {
+            nextEl: '.cards-wrap__next',
+            prevEl: '.cards-wrap__prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        breakpoints: {
+            1279: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            }
+        }
+    })
 });
