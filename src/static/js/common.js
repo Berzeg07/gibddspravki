fa236f79d920754117f4e86f8a837236596b33d6
Aslan <berezg86@gmail.com>
2020-12-05 12:41:01 +0300

    Футер, форма

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -25,4 +25,38 @@ $(document).ready(function () {
             }
         }
     })
+
+    $('.custom-select select').selectric()
+
+    ymaps.ready(init);
+
+    function init() {
+        var center = [55.59113656911934, 37.88662649999996];
+        var myMap = new ymaps.Map('placemap', {
+            center: center,
+            controls: [],
+            zoom: 8
+        }, {
+            searchControlProvider: 'yandex#search'
+
+        });
+
+        myMap.behaviors.disable('scrollZoom');
+
+        var myPlacemark = new ymaps.Placemark(center, {
+            // Свойства.
+            // Содержимое иконки, балуна и хинта.
+            balloonContent: 'улица Ивана Франко, 4к4',
+            hintContent: 'улица Ивана Франко, 4к4'
+        }, {
+            // Опции.
+            iconLayout: 'default#image',
+            // iconImageHref: 'img/map-ic.png',
+            // iconImageSize: [42, 42]
+            // preset: 'twirl#violetIcon'
+        });
+
+        myMap.geoObjects.add(myPlacemark);
+    }
+
 });
