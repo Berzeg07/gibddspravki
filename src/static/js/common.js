1ea93281efca6d9cce9b23cc812740b05e114442
Aslan <berezg86@gmail.com>
2020-12-05 13:41:22 +0300

    Верстка

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -26,6 +26,50 @@ $(document).ready(function () {
         }
     })
 
+    var myAdvSlider = undefined;
+
+    function initSwiper() {
+        var screenWidth = $(window).width();
+        if (screenWidth > 767 && myAdvSlider == undefined) {
+            var advSlider = new Swiper('.adv-gallery', {
+                slidesPerView: 5,
+                spaceBetween: 20,
+                loop: true,
+                observer: true,
+                observeParents: true,
+                navigation: {
+                    nextEl: '.adv-next',
+                    prevEl: '.adv-prev',
+                },
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                breakpoints: {
+                    1279: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                    },
+                    1599: {
+                        slidesPerView: 4,
+                        spaceBetween: 20,
+                    }
+                }
+            })
+        } else if (screenWidth < 768 && myAdvSlider != undefined) {
+            myAdvSlider.destroy();
+            myAdvSlider = undefined;
+        }
+    }
+
+    //Swiper plugin initialization
+    initSwiper();
+
+    //Swiper plugin initialization on window resize
+    $(window).on('resize', function () {
+        initSwiper();
+    });
+
     $('.custom-select select').selectric()
 
     ymaps.ready(init);
